4502cd1bf53259a87931b70dcdc52a0755b98bc6
Meerzulee <meerzulee@gmail.com>
2020-10-29 13:05:15 +0600

    Finish desktop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,187 @@
 <template>
-  <div class="home flex flex-col items-center">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="uppercase home">
+    <!-- header -->
+    <div class="relative w-full h-screen">
+      <div class="absolute top-0 right-0 z-0 h-screen">
+        <img src="@/assets/images/bg-header.png" class="w-full h-full" alt="" />
+      </div>
+      <div class="absolute top-0 left-0 z-0 h-screen">
+        <img src="@/assets/images/bg-left.png" class="w-full h-full" alt="" />
+      </div>
+      <!--  -->
+      <div
+        class="relative z-30 flex flex-col items-start px-40 py-16 text-left text-white"
+      >
+        <h1 class="mb-40 text-2xl font-bold">
+          4 ноября - 16 декабря <br />
+          участие бесплатное
+        </h1>
+        <h1 class="font-extrabold leading-snug text-7sm">
+          Прокачай <br />
+          свой бизнес
+        </h1>
+        <p class="mt-10 text-lg font-semibold">
+          Инкубационный онлайн - практикум от Astana Hub <br />
+          в г. Усть-Каменагорск
+        </p>
+        <a href="#" class="px-12 py-6 mt-24 text-xl rounded-2xl bg-primary"
+          >Оставить заявку</a
+        >
+      </div>
+    </div>
+    <div class="py-24">
+      <div class="flex flex-col max-w-6xl mx-auto">
+        <h1 class="text-4xl font-bold text-secondary">
+          Инкубационная программа от Astana hub - это:
+        </h1>
+        <div class="flex mt-32">
+          <div class="flex flex-col justify-center w-1/2 text-black">
+            <h1 class="text-4xl font-bold">6 недель</h1>
+            <p class="text-lg font-semibold">прокачки твоего бизнеса</p>
+          </div>
+          <div class="flex flex-col justify-center w-1/2 text-black">
+            <h1 class="text-4xl font-bold">15</h1>
+            <p class="text-lg font-semibold">стартапов и бизнесов</p>
+          </div>
+        </div>
+        <div class="flex mt-24 mb-24">
+          <div class="flex flex-col justify-center w-1/3 text-black">
+            <h1 class="text-4xl font-bold">50 часов</h1>
+            <p class="text-lg font-semibold">
+              индивидуальных <br />
+              консультаций <br />
+              от ведущих экспертов
+            </p>
+          </div>
+          <div class="flex flex-col justify-center w-1/3 text-black">
+            <h1 class="text-4xl font-bold">90 часов</h1>
+            <p class="text-lg font-semibold">
+              индивидуальных <br />
+              консультаций <br />
+              от ведущих экспертов
+            </p>
+          </div>
+          <div class="flex flex-col justify-center w-1/3 text-black">
+            <h1 class="text-4xl font-bold">50 часов</h1>
+            <p class="text-lg font-semibold">
+              практических <br />
+              воркшопов по развитию <br />
+              и масштабированию бизнеса
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="relative h-screen text-white">
+      <img
+        src="@/assets/images/banner.png"
+        class="absolute inset-0 z-0 w-full h-screen"
+        alt=""
+      />
+      <div class="relative max-w-5xl py-24 mx-auto">
+        <h1 class="mb-32 text-4xl font-bold text-center">Для кого</h1>
+        <div class="flex justify-between w-full text-left">
+          <div
+            class="flex flex-col px-12 py-10 shadow-lg h-480 w-420 bg-primary rounded-2xl"
+          >
+            <h1 class="mb-4 text-2xl font-extrabold leading-tight">
+              Для начинающих стартаперов
+            </h1>
+            <p class="card-text-p">
+              протестируешь и упакуешь свою бизнес - идею
+            </p>
+            <p class="card-text-p">
+              поймешь свою бизнес модель и сделаешь первые продажи
+            </p>
+            <p class="card-text-p">
+              соберешь команду, которая будет гореть твоим проектом
+            </p>
+            <p class="card-text-p">
+              получишь инструменты для быстрого роста бизнеса
+            </p>
+            <p class="card-text-p"></p>
+          </div>
+          <div
+            class="flex flex-col px-12 py-10 shadow-lg h-480 w-420 bg-primary rounded-2xl"
+          >
+            <h1 class="mb-4 text-2xl font-extrabold leading-tight">
+              Для действующих предпринимателей
+            </h1>
+
+            <p class="card-text-p">упакуешь свой бизнес</p>
+            <p class="card-text-p">
+              отстроешься от конкурентов и станешь №1 в своей нише
+            </p>
+            <p class="card-text-p">увеличишь продажи в бизнесе</p>
+            <p class="card-text-p">
+              получишь инструменты для масштабирования бизнеса
+            </p>
+            <p class="card-text-p">научишься эффективно управлять командой</p>
+            <p class="card-text-p">пробьешь свой личный финансовый потолок</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  -->
+    <div class="py-24">
+      <div class="flex flex-col px-64 mx-auto">
+        <h1 class="text-4xl font-bold text-secondary">
+          Ведущий трекер программы и партнер Astana Hub
+        </h1>
+        <div class="flex justify-between mt-24">
+          <div class="flex flex-col justify-start w-1/2 text-left text-black">
+            <h1 class="tracker-text">
+              <b>Эмир Касеев @emirforbes:</b> <br />
+              сертифицированный <br />
+              трекер <b>ФРИИ</b> г. Москва
+            </h1>
+            <h1 class="tracker-text">
+              Со-основатель бизнеса стоимостью <br />
+              <b>2 000 000 $</b>
+            </h1>
+            <h1 class="tracker-text">
+              Масштабировал и довел до <br />
+              результата <b>100</b> + бизнесов
+            </h1>
+            <h1 class="tracker-text">
+              Помог привлечь <b>500 000 000$</b> <br />
+              инвестиций в казахстанские стартапы
+            </h1>
+            <h1 class="tracker-text">
+              <b>Партнеры:</b> Astana hub,<br />
+              Startupbootcamp, ФРИИ, МФЦА, <br />
+              Nazarbayev University, Stratup Magic
+            </h1>
+          </div>
+          <div
+            class="flex flex-col justify-center w-1/2 text-black bg-gray-500"
+          ></div>
+        </div>
+      </div>
+    </div>
+    <div class="relative px-64 py-32 text-white">
+      <img
+        src="@/assets/images/footer.png"
+        class="absolute inset-0 w-full h-full"
+        alt=""
+      />
+      <div class="relative flex flex-col items-start text-left">
+        <h1 class="mb-10 text-5xl font-bold">
+          Прокачай свой бизнес <br />
+          вместе с Astana Hub
+        </h1>
+        <a href="#" class="px-12 py-6 text-xl rounded-2xl bg-primary"
+          >Участвовать</a
+        >
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    fontFamily: {
+      'body': ['Montserrat']
+    },
+    extend: {
+      fontSize: {
+        '7sm': '4.5rem'
+      },
+      colors: {
+        'primary': '#E7015E',
+        'secondary': '#32009D'
+      },
+      width: {
+        '420': '26.25rem'
+      },
+      height: {
+        '480': '30rem'
+      }
+
+    },
   },
   variants: {},
   plugins: [],
